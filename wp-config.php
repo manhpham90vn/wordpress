bca2524cc04559475000c146ac101a0e6ea9a609
Manh Pham <manhpham90vn@icloud.com>
2024-03-14 18:00:27 +0700

    sync

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', 'database' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
